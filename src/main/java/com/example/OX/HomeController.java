f6387eff946dda5f959d70f7a6e79210644fa6a1
WuddyS <jomyut7@gmail.com>
2019-02-21 02:15:34 +0700

    Commit: Add home mtethod

--- src/main/java/com/example/OX/HomeController.java
+++ src/main/java/com/example/OX/HomeController.java
@@ -1,23 +1,29 @@
 package com.example.OX;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ox")
 public class HomeController {
 
+    @GetMapping("/home")
+    public Map<String, String> home() {
+
+        HashMap<String, String> json = new HashMap<String, String>();
+        json.put("message", "hi!!");
+        return json;
+    }
+
     @GetMapping("/create-match")
-    public List  home(@RequestBody String player1, @RequestBody String player2) {
+    public Map home(@RequestBody String player1, @RequestBody String player2) {
 
-        List<?> list = new ArrayList<>();
+        HashMap<String, String> json = new HashMap<String, String>();
+        json.put("message", "Hi!!");
 
-        return list;
+        return json;
     }
 
 
